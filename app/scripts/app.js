a212931209f8578638417391fd55b6a8919415bf
Phillip Mispagel <atawmicpm@gmail.com>
2014-05-11 18:52:51 -0700

    programming weekend

---
+++ app/scripts/app.js
@@ -0,0 +1,36 @@
+/* global app:true */
+'use strict';
+
+var app = angular.module('angNewsApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'firebase'
+]);
+
+app.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/posts.html',
+      controller: 'PostsCtrl'
+    })
+    .when('/posts/:postId', {
+      templateUrl: 'views/showpost.html',
+      controller: 'PostViewCtrl'
+    })
+    .when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'AuthCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html',
+      controller: 'AuthCtrl'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+app.constant('FIREBASE_URL', 'https://ang-news-app.firebaseio.com/');
+
